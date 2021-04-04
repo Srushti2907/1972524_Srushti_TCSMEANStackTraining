96d3eeb818a4c6854f5475d4ffb19df9976e9dd6
srushtipatel <sap353@scarletmail.rutgers.edu>
2021-04-03 20:57:12 -0500

    quiz-application with json file

---
+++ Quiz/angular-exam/src/app/app.module.ts
@@ -0,0 +1,36 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ExamComponent } from './exam/exam.component';
+import{FormsModule,ReactiveFormsModule} from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+ import { AccordionModule } from 'ngx-bootstrap/accordion';
+
+ import { ModalModule } from 'ngx-bootstrap/modal';
+ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ import { ToastrModule } from 'ngx-toastr';
+ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ExamComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,ReactiveFormsModule,
+    HttpClientModule,
+   BrowserAnimationsModule,
+   ModalModule.forRoot(),
+   ToastrModule.forRoot(),
+    AccordionModule.forRoot(),
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
