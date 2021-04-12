8ae44f34fe1af98ca25c13ac5f5ea074ec4a7873
srushtipatel <sap353@scarletmail.rutgers.edu>
2021-04-12 16:45:30 -0500

    chatting with socket io project

---
+++ chat-application/server.js
@@ -0,0 +1,20 @@
+let app =require("express")();
+let http = require("http").Server(app);  //to load the library we have run port number using http module
+let io = require("socket.io")(http);
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+
+io.on("connection",(socket)=>{
+    console.log("client connected to chat-box application")
+
+socket.on('chat',(name,message)=>{
+        console.log(name)
+        console.log(message)
+       
+    })
+})
+
+
+http.listen(9090,()=>console.log('server running on port number 9090'))
